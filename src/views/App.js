c586d57e312ab62032fcd5f423f81960424379d2
LuVe132003 <vu.le132003@hcmut.edu.vn>
2023-10-29 09:57:28 +0700

    video3.2

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import MyComponent from './Example/myComponent';
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <MyComponent></MyComponent>
       </header>
     </div>
   );
