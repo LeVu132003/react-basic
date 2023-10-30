185c2f17606c7b73485bd7327e8124f25b2c422e
LuVe132003 <vu.le132003@hcmut.edu.vn>
2023-10-30 13:48:44 +0700

    todo-app

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.scss';
 import MyComponent from './Example/myComponent';
+import ListTodo from './Todos/ListTodo';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
@@ -8,9 +12,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" 
         alt="logo" />
-        
-        <MyComponent></MyComponent>
+        <p>Simple Todo App</p>
+        <ListTodo/>
       </header>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </div>
   );
 }
