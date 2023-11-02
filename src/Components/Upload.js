5cdcd6c36506e1ecb1471c13c6316c56910de8fd
LuVe132003 <vu.le132003@hcmut.edu.vn>
2023-11-02 11:52:20 +0700

    Upload edit save name

--- src/Components/Upload.js
+++ src/Components/Upload.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
 import './Upload.scss'
+import {toast } from 'react-toastify';
 function Upload() {
-
-  const [file, setFile] = useState()
+  const [state, setFile] = useState({ fileName: '' , fileSize: '' , fileType: '', onEdit: false })
+  let altText = '';
   function formatSizeUnits(bytes){
     if      (bytes >= 1073741824) { bytes = (bytes / 1073741824).toFixed(2) + " GB"; }
     else if (bytes >= 1048576)    { bytes = (bytes / 1048576).toFixed(2) + " MB"; }
@@ -13,24 +14,40 @@ function Upload() {
     return bytes;
   }
   function handleChange(event) {
-    console.log(event.target.files[0])
+    console.log(event.target.files[0].type)
     event.preventDefault();
-    setFile(event.target.files[0])
+    setFile({...state, fileName: (event.target.files[0].name).split('.').slice(0, -1).join('.'), fileSize: event.target.files[0].size, fileType: event.target.files[0].type})
+    toast.success('Upload successfully')
   }
   function handleEditName(event){
-    alert('Click me')
     event.preventDefault();
+    setFile({...state,onEdit : true})
+  }
+  function handleChangeName(event) {
+    console.log(event.target.value)
+    setFile({...state,fileName: event.target.value})    
+  }
+  function handleSaveName(event) {
+    event.preventDefault();
+    setFile({...state,onEdit : false})
   }
   return (
     <div className="Upload">
         <form>
-          { !file && <input type='file' onChange={handleChange}/>}
-          {file &&
+          { state.fileName === '' && state.fileSize === '' && <input type='file' onChange={handleChange}/>}
+          { state.fileSize !== '' &&
             <>
               <div className='file-edit-wrapper'>
-                <h1 className='file-title'>{file.name}</h1>
-                <span className='file-size'>{formatSizeUnits(file.size)}</span>
-                <button onClick = {(event)=> handleEditName(event)}>EditName</button>
+                {
+                  state.onEdit === false && state.fileSize != '' && <h1 className='file-title'>{state.fileName}</h1>
+                }
+                {
+                  state.onEdit === true && state.fileSize != '' && <input className='edit-name' type='text' value={state.fileName} onChange={(event)=>handleChangeName(event)}/>
+                }
+                { state.fileSize != '' && <span className='file-size'>{formatSizeUnits(state.fileSize)}</span>}
+                { state.fileSize != '' && <span className='file-type'>{state.fileType}</span>}
+                { state.fileSize != '' && state.onEdit === false && <button onClick = {(event)=> handleEditName(event)}>Edit Name</button>}
+                { state.fileSize != '' && state.onEdit === true && <button onClick = {(event)=> handleSaveName(event)}>Save Name</button>}
               </div>
             </>
           }
